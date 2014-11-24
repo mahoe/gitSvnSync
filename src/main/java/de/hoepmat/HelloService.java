28249ec4cf52c006f5915c500b6da43d8f76ebe1
Matthias Hoepfner <hoepmat@eclwsd041.xeop.de>
2014-11-24 12:03:04 +0100

    Initial commit

---
+++ src/main/java/de/hoepmat/HelloService.java
@@ -0,0 +1,10 @@
+package de.hoepmat;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloService {
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
