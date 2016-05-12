f20662746be2963b404c41a1108af708e83cde54
matthias.hoepfner <matthias.hoepfner@hoepfnersoftware.de>
2016-05-12 21:46:27 +0200

    GSS-1 Anzeige der letzten Message in der Übersicht

--- src/main/java/de/hoepmat/web/controller/WelcomeController.java
+++ src/main/java/de/hoepmat/web/controller/WelcomeController.java
@@ -1,5 +1,8 @@
 package de.hoepmat.web.controller;
 
+import de.hoepmat.web.StateHolder;
+import de.hoepmat.web.model.State;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.Map;
 
 /**
+ * The {@link WelcomeController} prepares the data to be presented if the user enters the welcome page.
  * Created by hoepfner on 22.04.2016.
  */
 @Controller
@@ -14,9 +18,17 @@ public class WelcomeController {
     @Value("${application.version}")
     private String applicationVersion;
 
+    @Autowired
+    private StateHolder stateHolder;
+
     @RequestMapping("/")
     public String welcome(Map<String,Object> model){
         model.put("application_version",applicationVersion);
+        final State state = stateHolder.getState();
+        String sb = "Message: " + state.getMessage() + "<BR>" +
+                "Reactivated at: " + state.getReactivateAt() + "<BR>" +
+                "Spend time for last sync: " + state.getTimeSpend();
+        model.put("application_state", sb);
         return "welcome";
     }
 }
