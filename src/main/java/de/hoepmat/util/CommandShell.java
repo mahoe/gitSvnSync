84bd54943e4601bf72b87fd447149f9c41465937
hoepmat <matthias.hoepfner.ext@deutsche-boerse.de>
2016-06-23 11:54:24 +0200

    add a timeout to the method that runs commands on a shell

--- src/main/java/de/hoepmat/util/CommandShell.java
+++ src/main/java/de/hoepmat/util/CommandShell.java
@@ -8,6 +8,7 @@ import org.springframework.util.StringUtils;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 /**
@@ -34,6 +35,8 @@ public class CommandShell {
     private String syncRepositoryPath;
 
     public ArrayList<String> runCommand(String command) {
+        LOGGER.info("runCommand('" + command + "')");
+        ArrayList<String> error_result = new ArrayList<>();
         ArrayList<String> result = new ArrayList<>();
         final Runtime runtime = Runtime.getRuntime();
         try {
@@ -43,37 +46,69 @@ public class CommandShell {
 
             final Process process = runtime.exec(commandLine, null, workDir);
 
+            LOGGER.info("getOutputLines() in error stream");
+            getOutputLines(error_result, process.getErrorStream());
+            LOGGER.info("getOutputLines() in input stream");
+            getOutputLines(result, process.getInputStream());
+
+            LOGGER.info("waitFor()");
+            boolean finished = waitForWithTimeout(process,1, TimeUnit.MINUTES);
+            if(!finished){
+                StringBuilder sb = new StringBuilder();
+                sb.append("Error on running shell command\n");
+                sb.append(StringUtils.collectionToDelimitedString(error_result, "\n"));
+                sb.append(StringUtils.collectionToDelimitedString(result, "\n"));
+                throw new RuntimeException( sb.toString());
+            }
+
+            final int exitValue = process.exitValue();
+
             StringBuilder sb = new StringBuilder("Result of command '");
             sb.append(commandLine)
                     .append("' in workdir [")
                     .append(workDir.getAbsolutePath())
                     .append("] was [")
-                    .append(process.exitValue())
+                    .append(exitValue)
                     .append("]");
             LOGGER.info(sb.toString());
 
-            getOutputLines(result, process.getErrorStream());
-
-            if (result.size() != 0) {
-                if ((process.exitValue() == 0)) {
-                    LOGGER.warning(result.toString());
+            if (error_result.size() != 0) {
+                if (exitValue == 0) {
+                    LOGGER.warning(error_result.toString());
+                    result.addAll(0,error_result);
                 } else {
                     sb = new StringBuilder();
                     sb.append("Error on running shell command\n");
-                    sb.append(StringUtils.collectionToDelimitedString(result, "\n"));
+                    sb.append(StringUtils.collectionToDelimitedString(error_result, "\n"));
                     throw new RuntimeException( sb.toString());
                 }
             }
 
-            getOutputLines(result, process.getInputStream());
-
-            process.waitFor();
         } catch (InterruptedException | IOException e) {
             mailService.sendErrorMessage(e.getMessage());
             throw new RuntimeException(e.toString());
         }
 
         return result;
+    }
+
+    private boolean waitForWithTimeout(Process process, long timeout, TimeUnit unit)
+            throws InterruptedException {
+        long startTime = System.nanoTime();
+        long rem = unit.toNanos(timeout);
+
+        do {
+            try {
+                process.exitValue();
+                return true;
+            }
+            catch (IllegalThreadStateException ex) {
+                if (rem > 0)
+                    Thread.sleep(Math.min(TimeUnit.NANOSECONDS.toMillis(rem) + 1, 100));
+            }
+            rem = unit.toNanos(timeout) - (System.nanoTime() - startTime);
+        } while (rem > 0);
+        return false;
     }
 
     private void getOutputLines(ArrayList<String> result, InputStream inputStream) throws IOException {
